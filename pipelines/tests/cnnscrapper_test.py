2de70375405c360e7d230d0acdf5222c91b10db0
Ahmad <mscs22004@itu.edu.pk>
2024-05-19 19:10:00 +0500

    Bug fix

--- pipelines/tests/cnnscrapper_test.py
+++ pipelines/tests/cnnscrapper_test.py
@@ -1,1 +1,14 @@
-assert (234 * 156) == 36504
+# pipelines/tests/test_cnnscrapper.py
+
+import pytest
+from job_units.CnnScrapper.entrypoint import add
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+
+if __name__ == "__main__":
+    pytest.main()
