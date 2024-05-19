653a90c6f70b9df498b49471c7f047c2a8560ec7
Ahmad <mscs22004@itu.edu.pk>
2024-05-19 19:38:33 +0500

    asdakd;a

--- pipelines/job_units/CnnScrapper/entrypoint.py
+++ pipelines/job_units/CnnScrapper/entrypoint.py
@@ -6,4 +6,4 @@ def add(a, b):
 
 
 if __name__ == "__main__":
-    print(add(3, 4))
+    print(add(4, 4))
--- pipelines/tests/cnnscrapper_test.py
+++ pipelines/tests/cnnscrapper_test.py
@@ -8,5 +8,6 @@ def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
 
+
 if __name__ == "__main__":
     pytest.main()
