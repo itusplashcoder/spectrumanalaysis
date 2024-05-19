1f857f3857ef92af9ad38de127c1fd8af1be09ad
Ahmad <mscs22004@itu.edu.pk>
2024-05-19 19:07:59 +0500

    Lint code

--- pipelines/job_units/CnnScrapper/entrypoint.py
+++ pipelines/job_units/CnnScrapper/entrypoint.py
@@ -1,7 +1,9 @@
 # pipelines/job_units/CnnScrapper/entrypoint.py
 
+
 def add(a, b):
     return a + b
+
 
 if __name__ == "__main__":
     print(add(2, 3))
--- pipelines/tests/__init__.py
+++ pipelines/tests/__init__.py
@@ -3,10 +3,12 @@
 import pytest
 from pipelines.job_units.CnnScrapper.entrypoint import add
 
+
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+
 
 if __name__ == "__main__":
     pytest.main()
